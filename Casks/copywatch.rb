220d514d5e0271beca22a016d89d92e04137ae6d
Ishaan Pilar <ishaanpilar98@gmail.com>
2026-07-17 02:58:12 +0530

    copywatch 1.1.0

--- Casks/copywatch.rb
+++ Casks/copywatch.rb
@@ -1,6 +1,6 @@
 cask "copywatch" do
-  version "1.0.1"
-  sha256 "e95400999144eb4283d99a87e1771ea8929046813dbec0ee9199c8ba7d974f81"
+  version "1.1.0"
+  sha256 "c28b08597ce7b848a574287d5bce3dcf8cfee62af53f2bca0cfc91885b9b6aae"
 
   url "https://github.com/ishaanpilar/CopyWatch/releases/download/v#{version}/CopyWatch-#{version}.dmg"
   name "CopyWatch"
